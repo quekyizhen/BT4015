d40c3826de09aed30e6a189897b354c4587a93bd
[zyloh] <loh.ziying@gmail.com>
2021-11-02 11:45:19 +0800

    added buffers for counting of HC, CC and TA

--- Data/Data Preprocessing NPC Point.R
+++ Data/Data Preprocessing NPC Point.R
@@ -2,7 +2,9 @@ library(tmap)
 library(rgdal)
 library(sf)
 library(GISTools)
+library(dplyr)
 require(rgdal)
+
 NPC_points <- st_as_sf(readOGR(dsn = "C:/Users/Zi Ying/Dropbox/Y4S1/BT4015/BT4015/Data/spf_establishments_2018", layer = "SPF_ESTABLISHMENTS_2018"))
 NPC_polygon <- readOGR(dsn = "C:/Users/Zi Ying/Dropbox/Y4S1/BT4015/BT4015/Data/npc boundary", layer = "singapore-police-force-npc-boundary-kml-polygon")
 
@@ -47,3 +49,105 @@ out <- st_intersection(NPC_polygon_sf, NPC_points)
 points_in_poly <- out[c("Name", "NPC_NAME", "NO", "BLDG", "TYPE")]
 points_in_poly = points_in_poly[order(points_in_poly$NPC_NAME),]
 tm_shape(NPC_polygon_sf) + tm_polygons(col="pink") + tm_shape(points_in_poly)+tm_dots("red") 
+
+
+# read in combined attributes based on NPC shp file
+pop_stats_by_npc <- st_as_sf(readOGR("C:/Users/Zi Ying/Dropbox/Y4S1/BT4015/BT4015/Data/NPC_all_attributes.shp"))
+tm_shape(pop_stats_by_npc) + tm_fill("Scl_P_D") + tm_shape(points_in_poly)+tm_dots("red") 
+
+#make sure the data is in the correct format for the spatstat package
+library(maptools)
+library(raster)
+library(spatstat)
+
+# polygon: make pop_stats_by_npc owin type 
+pop_stats_npc  <- readOGR("C:/Users/Zi Ying/Dropbox/Y4S1/BT4015/BT4015/Data/NPC_all_attributes.shp")
+#try using utm projection to convert coordinates to a projected one
+pop_stats_by_npc.utm <- spTransform(pop_stats_npc, CRS("+proj=utm +zone=48N +datum=WGS84 +no_defs"))
+
+pop_stats_by_npc_owin  <- as.owin(pop_stats_by_npc.utm)
+pop_stats_by_npc_owin.km <- rescale(pop_stats_by_npc_owin, 1000)
+
+
+# point: police station and police post
+intersected_points_spatial <- as(points_in_poly, "Spatial")
+
+intersected_points_spatial.utm <- spTransform(intersected_points_spatial, CRS("+proj=utm +zone=48N +datum=WGS84 +no_defs"))
+
+intersected_points_ppp  <- as.ppp(intersected_points_spatial.utm)
+marks(intersected_points_ppp) <- NULL
+intersected_points_ppp.km <- rescale(intersected_points_ppp, 1000)
+Window(intersected_points_ppp.km) <- intersected_points_ppp.km 
+
+# get population density raster layer
+#img  <- raster(pop_stats_by_npc)
+
+r <- raster(ncol=90, nrow=45)
+r.polys <- rasterize(pop_stats_npc, r, pop_stats_npc@data[["Scl_P_D"]], update = TRUE, updateValue = "NA")
+plot(r.polys)
+
+pop  <- as.im(r.polys)
+
+r2 <- raster(pop_stats_npc, res=10000)
+r2 <- rasterize(pop_stats_npc, field = Scl_P_D , r2)
+
+#do hypothesis testing for population density
+
+
+#read in hawker centre, cc and tourist attraction point data
+HC <- st_as_sf(readOGR("C:/Users/Zi Ying/Dropbox/Y4S1/BT4015/BT4015/Data/hawker-centres.shp"))
+CC <- st_as_sf(readOGR("C:/Users/Zi Ying/Dropbox/Y4S1/BT4015/BT4015/Data/community-clubs.shp"))
+TA <- st_as_sf(readOGR("C:/Users/Zi Ying/Dropbox/Y4S1/BT4015/BT4015/Data/tourism.shp"))
+
+# Buffer for each police station/police post
+npc_point_buf <- st_buffer(points_in_poly, dist = 1000)
+#npc_point_buf <- st_union(npc_point_buf)
+#npc_point_buf <- sf::st_make_valid(npc_point_buf)
+tm_shape(pop_stats_by_npc) + tm_fill("Scl_P_D") + tm_shape(npc_point_buf) + tm_polygons("pink") + tm_shape(HC) + tm_dots("black") + tm_shape(CC) + tm_dots("green") +  tm_shape(TA) + tm_dots("blue")
+
+#tm_shape(npc_point_buf) + tm_polygons("purple")
+
+#intersect buffer with npc polygon
+buf_int_npc <- st_intersection(npc_point_buf, NPC_polygon_sf) 
+tm_shape(buf_int_npc) + tm_polygons("blue") + tm_shape(NPC_polygon_sf) + tm_borders("yellow") +  tm_shape(HC) + tm_dots("black")
+
+#count of hc, cc, ta in each buffer
+hc_buf_intersection <- st_intersection(npc_point_buf, HC) #polygon, point
+hc_buf_count <- as.data.frame(hc_buf_intersection %>% group_by(NPC_NAME) %>% count())[, 1:2]
+names(hc_buf_count)[names(hc_buf_count) == 'n'] <- 'HC_in_buffer'
+
+cc_buf_intersection <- st_intersection(npc_point_buf, CC) #polygon, point
+cc_buf_count <- as.data.frame(cc_buf_intersection %>% group_by(NPC_NAME) %>% count())[, 1:2]
+names(cc_buf_count)[names(cc_buf_count) == 'n'] <- 'CC_in_buffer'
+
+ta_buf_intersection <- st_intersection(npc_point_buf, TA) #polygon, point
+ta_buf_count <- as.data.frame(ta_buf_intersection %>% group_by(NPC_NAME) %>% count())[, 1:2]
+names(ta_buf_count)[names(ta_buf_count) == 'n'] <- 'TA_in_buffer'
+
+#colnames(npc@data) = col_names$column_names
+#Merge the counts into 1 df based on NPC point name
+hc_cc_ta_in_buf <- merge(hc_buf_count, cc_buf_count, by = 'NPC_NAME', all.x=TRUE)
+hc_cc_ta_in_buf <- merge(hc_cc_ta_in_buf, ta_buf_count, by = 'NPC_NAME', all.x=TRUE)
+
+#count of hc, cc, ta in each NPC polygon
+# Count the number of HC per NPC
+hc_npc_intersection <- st_intersection(NPC_polygon_sf, HC) #polygon, point
+hc_npc_count <- as.data.frame(hc_npc_intersection %>% group_by(NPC_NAME) %>% count())[, 1:2]
+names(hc_npc_count)[names(hc_npc_count) == 'n'] <- 'HC_in_NPC_polygon'
+
+cc_npc_intersection <- st_intersection(NPC_polygon_sf, CC) #polygon, point
+cc_npc_count <- as.data.frame(cc_npc_intersection %>% group_by(NPC_NAME) %>% count())[, 1:2]
+names(cc_npc_count)[names(cc_npc_count) == 'n'] <- 'CC_in_NPC_polygon'
+
+ta_npc_intersection <- st_intersection(NPC_polygon_sf, TA) #polygon, point
+ta_npc_count <- as.data.frame(ta_npc_intersection %>% group_by(NPC_NAME) %>% count())[, 1:2]
+names(ta_npc_count)[names(ta_npc_count) == 'n'] <- 'TA_in_NPC_polygon'
+
+final <- merge(hc_cc_ta_in_buf, hc_npc_count, by = 'NPC_NAME', all.x=TRUE)
+final <- merge(final, cc_npc_count, by = 'NPC_NAME', all.x=TRUE)
+final <- merge(final, ta_npc_count, by = 'NPC_NAME', all.x=TRUE)
+
+
+final$HC_percent <- final$HC_in_buffer/final$HC_in_NPC_polygon*100
+final$CC_percent <- final$CC_in_buffer/final$CC_in_NPC_polygon*100
+final$TA_percent <- final$TA_in_buffer/final$TA_in_NPC_polygon*100
