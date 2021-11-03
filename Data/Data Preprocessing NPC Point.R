2b67b69d0e437f8acff281f5f31bf29e17fba835
[zyloh] <loh.ziying@gmail.com>
2021-11-03 11:20:38 +0800

    added hypothesis testing

--- Data/Data Preprocessing NPC Point.R
+++ Data/Data Preprocessing NPC Point.R
@@ -77,23 +77,50 @@ intersected_points_spatial.utm <- spTransform(intersected_points_spatial, CRS("+
 intersected_points_ppp  <- as.ppp(intersected_points_spatial.utm)
 marks(intersected_points_ppp) <- NULL
 intersected_points_ppp.km <- rescale(intersected_points_ppp, 1000)
-Window(intersected_points_ppp.km) <- intersected_points_ppp.km 
+#Window(intersected_points_ppp.km) <- intersected_points_ppp.km
+#this line throws an error but seems to not be necessary?
 
 # get population density raster layer
 #img  <- raster(pop_stats_by_npc)
 
-r <- raster(ncol=90, nrow=45)
+r <- raster(pop_stats_npc)
+nrow(r) <-1000
+ncol(r) <- 1000
+
 r.polys <- rasterize(pop_stats_npc, r, pop_stats_npc@data[["Scl_P_D"]], update = TRUE, updateValue = "NA")
+
 plot(r.polys)
 
 pop  <- as.im(r.polys)
+hist(pop, main=NULL, las=1)
+pop.km    <- rescale(pop, 1000, "km")
 
-r2 <- raster(pop_stats_npc, res=10000)
-r2 <- rasterize(pop_stats_npc, field = Scl_P_D , r2)
+pop.lg <- log(pop)
+hist(pop.lg, main=NULL, las=1) # not much difference in shape? is it really better?
+pop.lg.km <- rescale(pop.lg, 1000, "km")
+
 
 #do hypothesis testing for population density
+#model with pop density as the function
+n <- 599L
+ann.r <- vector(length=n)
+for (i in 1:n){
+  rand.p   <- rpoint(n=intersected_points_ppp.km$n, f=pop.km, win=pop_stats_by_npc_owin.km) 
+  ann.r[i] <- mean(nndist(rand.p, k=1))
+}
 
+#Window(rand.p) <- pop_stats_by_npc_owin.km
+plot(rand.p, pch=16, main=NULL, cols=rgb(0,0,0,0.5))
 
+#observed
+ann.p <- mean(nndist(intersected_points_ppp.km, k=1))
+ann.p
+
+#histogram
+hist(ann.r, main=NULL, las=1, col="bisque", xlim=range(ann.p, ann.r))
+abline(v=ann.p, col="blue")
+
+## HC, CC, TA Coverage in each NPC buffer
 #read in hawker centre, cc and tourist attraction point data
 HC <- st_as_sf(readOGR("C:/Users/Zi Ying/Dropbox/Y4S1/BT4015/BT4015/Data/hawker-centres.shp"))
 CC <- st_as_sf(readOGR("C:/Users/Zi Ying/Dropbox/Y4S1/BT4015/BT4015/Data/community-clubs.shp"))
@@ -101,33 +128,41 @@ TA <- st_as_sf(readOGR("C:/Users/Zi Ying/Dropbox/Y4S1/BT4015/BT4015/Data/tourism
 
 # Buffer for each police station/police post
 npc_point_buf <- st_buffer(points_in_poly, dist = 1000)
-#npc_point_buf <- st_union(npc_point_buf)
-#npc_point_buf <- sf::st_make_valid(npc_point_buf)
+npc_point_buf <- st_union(npc_point_buf)
+npc_point_buf <- sf::st_make_valid(npc_point_buf)
 tm_shape(pop_stats_by_npc) + tm_fill("Scl_P_D") + tm_shape(npc_point_buf) + tm_polygons("pink") + tm_shape(HC) + tm_dots("black") + tm_shape(CC) + tm_dots("green") +  tm_shape(TA) + tm_dots("blue")
 
 #tm_shape(npc_point_buf) + tm_polygons("purple")
 
-#intersect buffer with npc polygon
-buf_int_npc <- st_intersection(npc_point_buf, NPC_polygon_sf) 
 tm_shape(buf_int_npc) + tm_polygons("blue") + tm_shape(NPC_polygon_sf) + tm_borders("yellow") +  tm_shape(HC) + tm_dots("black")
 
 #count of hc, cc, ta in each buffer
-hc_buf_intersection <- st_intersection(npc_point_buf, HC) #polygon, point
-hc_buf_count <- as.data.frame(hc_buf_intersection %>% group_by(NPC_NAME) %>% count())[, 1:2]
+hc_buf_intersection <- st_intersection(HC, npc_point_buf) #polygon, point
+hc_covered <- st_intersection(NPC_polygon_sf, hc_buf_intersection)
+
+tm_shape(NPC_polygon_sf) + tm_borders("yellow")+ tm_shape(hc_covered) + tm_dots("purple")
+hc_buf_count <- as.data.frame(hc_covered %>% group_by(NPC_NAME) %>% count())[, 1:2]
 names(hc_buf_count)[names(hc_buf_count) == 'n'] <- 'HC_in_buffer'
 
-cc_buf_intersection <- st_intersection(npc_point_buf, CC) #polygon, point
-cc_buf_count <- as.data.frame(cc_buf_intersection %>% group_by(NPC_NAME) %>% count())[, 1:2]
+#CC
+cc_buf_intersection <- st_intersection(CC, npc_point_buf) #polygon, point
+cc_covered <- st_intersection(NPC_polygon_sf, cc_buf_intersection)
+tm_shape(NPC_polygon_sf) + tm_borders("yellow")+ tm_shape(cc_covered) + tm_dots("green")
+
+cc_buf_count <- as.data.frame(cc_covered %>% group_by(NPC_NAME) %>% count())[, 1:2]
 names(cc_buf_count)[names(cc_buf_count) == 'n'] <- 'CC_in_buffer'
 
-ta_buf_intersection <- st_intersection(npc_point_buf, TA) #polygon, point
-ta_buf_count <- as.data.frame(ta_buf_intersection %>% group_by(NPC_NAME) %>% count())[, 1:2]
+#TA
+ta_buf_intersection <- st_intersection(TA, npc_point_buf) #polygon, point
+ta_covered <- st_intersection(NPC_polygon_sf, ta_buf_intersection)
+
+ta_buf_count <- as.data.frame(ta_covered %>% group_by(NPC_NAME) %>% count())[, 1:2]
 names(ta_buf_count)[names(ta_buf_count) == 'n'] <- 'TA_in_buffer'
 
-#colnames(npc@data) = col_names$column_names
 #Merge the counts into 1 df based on NPC point name
 hc_cc_ta_in_buf <- merge(hc_buf_count, cc_buf_count, by = 'NPC_NAME', all.x=TRUE)
 hc_cc_ta_in_buf <- merge(hc_cc_ta_in_buf, ta_buf_count, by = 'NPC_NAME', all.x=TRUE)
+hc_cc_ta_in_buf[is.na(hc_cc_ta_in_buf)] <- 0
 
 #count of hc, cc, ta in each NPC polygon
 # Count the number of HC per NPC
@@ -147,7 +182,8 @@ final <- merge(hc_cc_ta_in_buf, hc_npc_count, by = 'NPC_NAME', all.x=TRUE)
 final <- merge(final, cc_npc_count, by = 'NPC_NAME', all.x=TRUE)
 final <- merge(final, ta_npc_count, by = 'NPC_NAME', all.x=TRUE)
 
-
 final$HC_percent <- final$HC_in_buffer/final$HC_in_NPC_polygon*100
 final$CC_percent <- final$CC_in_buffer/final$CC_in_NPC_polygon*100
 final$TA_percent <- final$TA_in_buffer/final$TA_in_NPC_polygon*100
+
+final[is.na(final)] <- 0
