41214aece5aa6ca0d91ea0c3a3cb289897208efa
[zyloh] <loh.ziying@gmail.com>
2021-11-08 16:35:17 +0800

    clean up code and edit breaks for pop density quadrat

--- Data/Data Preprocessing NPC Point.R
+++ Data/Data Preprocessing NPC Point.R
@@ -4,6 +4,7 @@ library(sf)
 library(GISTools)
 library(dplyr)
 require(rgdal)
+tmap_mode('plot')
 
 NPC_points <- st_as_sf(readOGR(dsn = "C:/Users/Zi Ying/Dropbox/Y4S1/BT4015/BT4015/Data/spf_establishments_2018", layer = "SPF_ESTABLISHMENTS_2018"))
 NPC_polygon <- readOGR(dsn = "C:/Users/Zi Ying/Dropbox/Y4S1/BT4015/BT4015/Data/npc boundary", layer = "singapore-police-force-npc-boundary-kml-polygon")
@@ -24,13 +25,11 @@ st_is_valid(NPC_polygon_sf)
 NPC_polygon_sf <- st_make_valid(NPC_polygon_sf)
 st_is_valid(NPC_polygon_sf)
 
-tmap_mode('view')
 tm_shape(NPC_polygon_sf) + tm_polygons(col="pink") + tm_shape(NPC_points)+tm_dots("blue")
 
 
-#remove airport polygon
-
-NPC_polygon_sf = NPC_polygon_sf[!NPC_polygon_sf$Name == "kml_25",]
+#remove airport and jurong island polygon
+NPC_polygon_sf = NPC_polygon_sf[!(NPC_polygon_sf$Name == "kml_25" | NPC_polygon_sf$Name == "kml_3"),]
 nrow(NPC_polygon_sf)
 View(NPC_polygon_sf)
 tm_shape(NPC_polygon_sf) + tm_polygons(col="pink")
@@ -39,21 +38,27 @@ tm_shape(NPC_polygon_sf) + tm_polygons(col="pink")
 NPC_points = NPC_points[!(NPC_points$NO == "94"| NPC_points$NO == "99"),]
 nrow(NPC_points)
 View(NPC_points)
-tm_shape(NPC_points)+tm_dots("blue")
-
 
 #find intersection of npc points and npc boundaries
 #get list of points in each npc
-
 out <- st_intersection(NPC_polygon_sf, NPC_points)
 points_in_poly <- out[c("Name", "NPC_NAME", "NO", "BLDG", "TYPE")]
 points_in_poly = points_in_poly[order(points_in_poly$NPC_NAME),]
-tm_shape(NPC_polygon_sf) + tm_polygons(col="pink") + tm_shape(points_in_poly)+tm_dots("red") 
 
+tm_shape(NPC_polygon_sf) + tm_polygons(col="pink") + tm_shape(points_in_poly)+tm_dots("red") 
 
 # read in combined attributes based on NPC shp file
 pop_stats_by_npc <- st_as_sf(readOGR("C:/Users/Zi Ying/Dropbox/Y4S1/BT4015/BT4015/Data/NPC_all_attributes.shp"))
-tm_shape(pop_stats_by_npc) + tm_fill("Scl_P_D") + tm_shape(points_in_poly)+tm_dots("red") 
+col_names <- read.csv("C:/Users/Zi Ying/Dropbox/Y4S1/BT4015/BT4015/Data/NPC_all_attributes_col_names.csv")
+colnames(pop_stats_by_npc) = col_names$column_names
+
+tm_shape(pop_stats_by_npc) + tm_fill("Scaled.Population.Density", title = "Population Density") + 
+  tm_shape(points_in_poly)+tm_dots("red", size = 0.1)+
+  tm_layout(legend.title.size = 1,
+          legend.text.size = 0.5,
+          legend.bg.alpha = 1, legend.outside = TRUE) + 
+  tm_scale_bar(position=c("right", "bottom")) + 
+  tm_compass(type="8star", position=c("right", "top"), show.labels = 3, size=2, text.size=0.5)
 
 #make sure the data is in the correct format for the spatstat package
 library(maptools)
@@ -95,7 +100,6 @@ pop.lg <- log(pop)
 hist(pop.lg, main=NULL, las=1) # not much difference in shape? is it really better?
 pop.lg.km <- rescale(pop.lg, 1000, "km")
 
-
 #do hypothesis testing for population density
 #model with pop density as the function
 n <- 599L
@@ -136,6 +140,40 @@ intersected_points_ppp.km$n / area(pop_stats_by_npc_owin.km)
 #to see if they are significantly different from each other
 anova(PPM0, PPM1, test="LRT")
 
+
+#Descriptive Analysis of NPC Points
+#Density
+#merge points_in_poly with pop_stats_by_npc to get area of each polygon 
+
+pop_stats_area <- as.data.frame(pop_stats_by_npc[c('NPC_NAME', 'NPC.Area.in.km2', 'crime.rate.npc.area')])
+npc_with_area <- merge(pop_stats_area, points_in_poly, by = 'NPC_NAME', all.x=TRUE)
+npc_count <- as.data.frame(npc_with_area %>% group_by(NPC_NAME) %>% count())[, 1:2]
+names(npc_count)[names(npc_count) == 'n'] <- 'NPC_COUNT'
+
+npc_count_with_area <- merge(npc_count, pop_stats_area, by = 'NPC_NAME', all.x=TRUE)
+npc_count_with_area$NPC_Density <- npc_count_with_area$NPC_COUNT/npc_count_with_area$NPC.Area.in.km2
+
+tm_shape(st_as_sf(npc_count_with_area)) +tm_fill("crime.rate.npc.area", title = "Crime Rate by NPC") + 
+  tm_bubbles(size="NPC_Density", col="blue")+
+  tm_borders('black') + 
+  tm_layout(legend.title.size = 1,
+            legend.text.size = 0.7,
+            legend.bg.alpha = 1, legend.outside = TRUE)+ 
+  tm_scale_bar(position=c("right", "bottom")) + 
+  tm_compass(type="8star", position=c("right", "top"), show.labels = 3, size=2, text.size=0.5)
+
+#Quadrat by population density
+brk  <- c( -Inf, 8, 9, 10, Inf)  # Define the breaks
+Zcut <- cut(pop.lg.km, breaks=brk, labels=1:4)  # Classify the raster
+E    <- tess(image=Zcut)  # Create a tesselated surface
+plot(E, main="", las=1)
+Q   <- quadratcount(intersected_points_ppp.km, tess = E)  # Tally counts
+Q.d <- intensity(Q)  # Compute density
+Q.d
+cl <- interp.colours(c("lightyellow", "orange" ,"red"), E$n)
+plot(intensity(Q, image=TRUE), las=1, col=cl, main=NULL)
+plot(intersected_points_ppp.km, pch=20, cex=0.6, col=rgb(0,0,0,.5), add=TRUE)
+
 ## HC, CC, TA Coverage in each NPC buffer
 #read in hawker centre, cc and tourist attraction point data
 HC <- st_as_sf(readOGR("C:/Users/Zi Ying/Dropbox/Y4S1/BT4015/BT4015/Data/hawker-centres.shp"))
@@ -144,9 +182,12 @@ TA <- st_as_sf(readOGR("C:/Users/Zi Ying/Dropbox/Y4S1/BT4015/BT4015/Data/tourism
 
 # Buffer for each police station/police post
 npc_point_buf <- st_buffer(points_in_poly, dist = 1000)
+
 #undissolved buffer
-tmap_mode('plot')
-tm_shape(NPC_polygon_sf) + tm_borders('black') + tm_shape(npc_point_buf) + tm_polygons("blue", alpha = 0.5) + tm_layout(title= 'Undissolved NPC Buffer', title.position = c('right', 'top'), title.size = 0.8) +tm_layout(legend.title.size = 1,
+tm_shape(NPC_polygon_sf) + tm_borders('black') + 
+  tm_shape(npc_point_buf) + tm_polygons("blue", alpha = 0.5) +
+  tm_shape(points_in_poly)+tm_dots("red", size = 0.1) + 
+  tm_layout(title= 'Undissolved Buffer 1km radius around each NPC', title.position = c('right', 'top'), title.size = 0.8) +tm_layout(legend.title.size = 1,
              legend.text.size = 0.7,
              legend.bg.alpha = 1, legend.outside = TRUE)+ 
   tm_scale_bar(position=c("right", "bottom")) + 
@@ -157,7 +198,10 @@ npc_point_buf <- sf::st_make_valid(npc_point_buf)
 #tm_shape(pop_stats_by_npc) + tm_fill("Scl_P_D") + tm_shape(npc_point_buf) + tm_polygons("pink") + tm_shape(HC) + tm_dots("black") + tm_shape(CC) + tm_dots("green") +  tm_shape(TA) + tm_dots("blue")
 
 #plot hawker centre
-tm_shape(NPC_polygon_sf) + tm_borders('black') + tm_shape(npc_point_buf) + tm_polygons("blue", alpha = 0.5)+ tm_shape(HC) + tm_dots("black", size = 0.1) + tm_layout(title= 'Hawker Centres in Dissolved NPC Buffer', title.position = c('right', 'top'), title.size = 0.8)+ tm_layout(legend.title.size = 0.5,
+tm_shape(NPC_polygon_sf) + tm_borders('black') + 
+  tm_shape(npc_point_buf) + tm_polygons("blue", alpha = 0.5)+ 
+  tm_shape(HC) + tm_dots("black", size = 0.1) + 
+  tm_layout(title= 'Hawker Centres in Dissolved NPC Buffer', title.position = c('right', 'top'), title.size = 0.8)+ tm_layout(legend.title.size = 0.5,
             legend.text.size = 0.7,
             legend.bg.alpha = 1, legend.outside = TRUE)+ 
   tm_scale_bar(position=c("right", "bottom")) + 
@@ -171,11 +215,15 @@ hc_buf_count <- as.data.frame(hc_covered %>% group_by(NPC_NAME) %>% count())[, 1
 names(hc_buf_count)[names(hc_buf_count) == 'n'] <- 'HC_in_buffer'
 
 #plot CC
-tm_shape(NPC_polygon_sf) + tm_borders('black') + tm_shape(npc_point_buf) + tm_polygons("blue", alpha = 0.5)+ tm_shape(CC) + tm_dots("red", size = 0.1) + tm_layout(title= 'Community Centres in Dissolved NPC Buffer', title.position = c('right', 'top'), title.size = 0.8) + tm_layout(legend.title.size = 1,
+tm_shape(NPC_polygon_sf) + tm_borders('black') + 
+  tm_shape(npc_point_buf) + tm_polygons("blue", alpha = 0.5)+ 
+  tm_shape(CC) + tm_dots("pink", size = 0.1) + 
+  tm_layout(title= 'Community Centres in Dissolved NPC Buffer', title.position = c('right', 'top'), title.size = 0.8) + tm_layout(legend.title.size = 1,
             legend.text.size = 0.7,
             legend.bg.alpha = 1, legend.outside = TRUE)+ 
   tm_scale_bar(position=c("right", "bottom")) + 
   tm_compass(type="8star", position=c("right", "top"), show.labels = 3, size=2, text.size=0.5)
+
 #CC
 cc_buf_intersection <- st_intersection(CC, npc_point_buf) #polygon, point
 cc_covered <- st_intersection(NPC_polygon_sf, cc_buf_intersection)
@@ -190,6 +238,7 @@ tm_shape(NPC_polygon_sf) + tm_borders('black') + tm_shape(npc_point_buf) + tm_po
             legend.bg.alpha = 1, legend.outside = TRUE)+ 
   tm_scale_bar(position=c("right", "bottom")) + 
   tm_compass(type="8star", position=c("right", "top"), show.labels = 3, size=2, text.size=0.5)
+
 #TA
 ta_buf_intersection <- st_intersection(TA, npc_point_buf) #polygon, point
 ta_covered <- st_intersection(NPC_polygon_sf, ta_buf_intersection)
